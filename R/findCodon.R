6c9079bedafc487b38ab08e3274c32990855e25f
oacar <omeracar0@gmail.com>
2018-11-05 12:55:45 -0500

    an check only X codons after the ORF region

--- R/findCodon.R
+++ R/findCodon.R
@@ -2,7 +2,7 @@
 
 #'find Codon
 #'Searches for start or stop codons
-#'For start codon search the input sequence should end right before the position where it alignes with start codon of proto-gene ORF 
+#'For start codon search the input sequence should end right before the position where it alignes with start codon of proto-gene ORF
 #'For stop codon search the input sequence should start from the position where it alignes with start codon of proto-gene ORF so for actual position on the DNA alignment (that start - 1 ) should be added the returned value
 #'@param sequence DNAString object
 #'@param codonSet ('ATG') for start codon or ('TAA', 'TAG', 'TGA') for stop codon search
@@ -11,7 +11,7 @@
 #'@export
 
 findCodon<-function(sequence, codonSet){
-  
+
   if(codonSet!='ATG' && any(codonSet!=c('TAA', 'TAG', 'TGA') )){
     stop('codonSet should be either \'ATG\' or c(\'TAA\', \'TAG\', \'TGA\')')
   }
@@ -20,7 +20,7 @@ findCodon<-function(sequence, codonSet){
   min_lengthGaps<-Inf
   i<-1
   seqWoGaps<-turnWoGaps(sequence)
-  
+
   if(any(codonSet=='ATG')){
     allCoordinates<-gregexpr(regSeq(codonSet),seqWoGaps)[[1]]
     allCoordinatesWGaps<-gregexpr(regSeq(codonSet),sequence)[[1]]
@@ -29,12 +29,12 @@ findCodon<-function(sequence, codonSet){
       if((diff_)%%3==0 && diff_<min_){
         min_<-diff_
         min_i<-allCoordinatesWGaps[i]
-        
+
       }
       i=i+1
     }
     return(min_i)
-    
+
   } else {
     for( codon_ in codonSet){
       allCoordinates<-gregexpr(regSeq(codon_),seqWoGaps)[[1]]
@@ -42,7 +42,7 @@ findCodon<-function(sequence, codonSet){
       i<-length(allCoordinates)
       while(i>0){
         diff_<-allCoordinates[i]-1
-        if((diff_)%%3==0 && diff_<min_){
+        if((diff_)%%3==0 && diff_<min_ && diff_>length(seqWoGaps)){
           min_<-diff_
           min_index<-i
           min_i<-allCoordinates_withGaps[i]

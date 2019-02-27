40bf401ce56433e6d664f9d87fc1653957d27517
oacar <omeracar0@gmail.com>
2019-02-27 10:07:27 -0500

    resolve conflicts

--- R/findBestOverlap.R
+++ R/findBestOverlap.R
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #'Finds best overlapping ORF in terms of the number of identical aminoacids
-=======
-#'
->>>>>>> c1655aa13be547d7cec4b70c6fa5291cc3ca1da2
 #'@param DNAStr DNA alignment of syntenic block. This is the full alignment not subalignment
 #'@param j this is the iterator for the ID of species of interest on DNAStr and types vector
 #'@param r this is the range parameter. This specifies how far from the proto-gene region will be taken into account for ORF finding default =200
@@ -16,7 +12,6 @@
 #'
 #'@export
 
-<<<<<<< HEAD
 findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , k=NULL) {
   k <- ifelse(is.null(k),r,k)
   subseq <- subseq(DNAStr[[j]],start=ifelse(start-r<1,1,start-r),end=ifelse(stop+k<length(DNAStr[[j]]),stop+k,length(DNAStr[[j]])))
@@ -36,15 +31,6 @@ findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , k=NULL) 
   bestAA <- NULL
   bestDna <- NULL
   bestAAsmall <- NULL
-=======
-findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , u=NULL) {
-  u <- ifelse(is.null(u),r,u)
-  subseq <- subseq(DNAStr[[j]],start=start-r,end=stop+u)
-  ranges <- findOverlappingOrfs(subseq,range = IRanges(r,stop-start+r))
-  if(length(ranges)==0) return(FALSE)
-  score=0
-  best <- NULL
->>>>>>> c1655aa13be547d7cec4b70c6fa5291cc3ca1da2
   for( u in 1:length(ranges)){
     startPosRanges <- start(ranges)[u]
     stopPosRanges <- end(ranges)[u]
@@ -52,7 +38,6 @@ findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , u=NULL) 
     # names(sequenceRanges) <- types[[j]]
     if(startPosRanges<r+1){
       smorfSubSeq <- subseq(DNAStr[1],start-r-1+startPosRanges,start-r-1+stopPosRanges)
-<<<<<<< HEAD
       place <- str_locate(turnWoGaps(smorfSubSeq[[1]]),as.character(subseq(ygeneSeq,1,10))[[1]])
       if(any(is.na(place))){
         next
@@ -93,38 +78,12 @@ findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , u=NULL) 
       warning(paste(names(ygeneSeq),types[j],'has a problematic overlapping ORF. Nice to check'))
       next
     }
-=======
-      place <- str_locate(turnWoGaps(smorfSubSeq[[1]]),as.character(subseq(ygeneSeq,1,7))[[1]])
-      longer=T
-      if(place[1,1]%%3==2){
-        smorfSubSeq <- subseq(DNAStr[1],start-r-2+startPosRanges,start-r-1+stopPosRanges)
-
-      }else if(place[1,1]%%3==0){
-        smorfSubSeq <- subseq(DNAStr[1],start-r-3+startPosRanges,start-r-1+stopPosRanges)
-      }
-    }else{
-      smorfSubSeq <- subseq(DNAStr[1],start-r-1+startPosRanges,start-r-1+stopPosRanges)
-      place <- str_locate(as.character(ygeneSeq),turnWoGaps(subseq(smorfSubSeq[[1]],1,ifelse(width(smorfSubSeq)<10,width(smorfSubSeq),10))))
-      longer=F
-      if(place[1,1]%%3==2){
-        smorfSubSeq <- subseq(DNAStr[1],start-r-2+startPosRanges,start-r-1+stopPosRanges)
-
-      }else if(place[1,1]%%3==0){
-        smorfSubSeq <- subseq(DNAStr[1],start-r-3+startPosRanges,start-r-1+stopPosRanges)
-      }
-    }
-    dnaSet <- append(DNAStringSet(sequenceRanges),smorfSubSeq)
-    names(dnaSet)[1] <- types[j]
-    aaRanges <- aaTranslation(dnaSet,DNAStr[c(j,1)])
-    aaRangesSmall <- findSmallFrame(aaRanges,2,aaRanges[[2]],overlap=T,longer=longer)[c(2,1)]
->>>>>>> c1655aa13be547d7cec4b70c6fa5291cc3ca1da2
     newscore <- calcIdentity(aaRangesSmall)*width(aaRanges[1])
     if(score<newscore[2,1]){
       score=newscore[2,1]
       bestAA <- aaRanges[c(2,1)]
       bestAAsmall <- aaRangesSmall
       bestDna <- dnaSet[c(2,1)]
-<<<<<<< HEAD
     }else if(score==newscore[2,1]){
       if(length(bestAA[[2]])<length(aaRanges[[1]])){
         score=newscore[2,1]
@@ -144,9 +103,3 @@ findBestOverlap <- function(DNAStr, j, r,start, stop, ygeneSeq, types , u=NULL) 
 # rm(bestDna)
 # rm(bestAA)
 # rm(bestAAsmall)
-=======
-    }
-  }
-  return(list(dna=bestDna,aa=bestAA, aaOverlap=bestAAsmall))
-}
->>>>>>> c1655aa13be547d7cec4b70c6fa5291cc3ca1da2

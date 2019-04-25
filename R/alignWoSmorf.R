4f715c718c87a5c5560d1455922d49e4668963cd
oacar <omeracar0@gmail.com>
2019-04-25 15:54:28 -0400

    user can specify if given input is already aligned or not

--- R/alignWoSmorf.R
+++ R/alignWoSmorf.R
@@ -4,6 +4,7 @@
 #'@param sequenceSet The sequences of syntenic blocks as DNAStringSet object. At leas 3 sequences are needed and last sequence should be the proto-gene
 #'@param algorithm this function uses msa package for alignment which can use c("ClustalW", "ClustalOmega", "Muscle") as alignment algorithms. I used 'Muscle' as default. There is also standalone muscle package which is also used in this package
 #'@param smorfSeq if this is given, the {sequenceSet} is taken as full syntenic blocks, if not the last sequence of the {sequenceSet} is taken as the orf of interest sequence
+#'@param aligned if this is TRUE, then the given sequences will be taken as aligned, so no realignment will be applied.
 #'@return list of alignment object as DNAStringSet with start and stop positions of proto-gene ORF on the aligned Scer sequence
 #'@import Biostrings
 #'@import msa
@@ -11,7 +12,7 @@
 #'@export
 #'
 
-alignWoSmorf <- function(sequenceSet, algorithm='Muscle',smorfSeq=NULL) {
+alignWoSmorf <- function(sequenceSet, algorithm='Muscle',smorfSeq=NULL, aligned=F) {
 
   ## requirement : at least 3 sequences
   l<-length(sequenceSet)
@@ -28,16 +29,20 @@ alignWoSmorf <- function(sequenceSet, algorithm='Muscle',smorfSeq=NULL) {
     reg<-regSeq(smorfSeq[[1]])
 
   }
-
+if(aligned==F){
   alignment <- tryCatch({alignment<-msa(sub, algorithm, order='input', type = 'dna')
-                          alignment<-DNAStringSet(alignment)
-                          alignment<-chartr('N','-',alignment)
-                          alignment},
-                        error=function(cond){
-                            alignment <- muscle(sub,quiet = T)
-                            alignment <- DNAStringSet(alignment)
-                            alignment
-                          })
+  alignment<-DNAStringSet(alignment)
+  alignment<-chartr('N','-',alignment)
+  alignment},
+  error=function(cond){
+    alignment <- muscle(sub,quiet = T)
+    alignment <- DNAStringSet(alignment)
+    alignment
+  })
+}else{
+  alignment=sub
+}
+  
 
 
   regexpr_result<-regexpr(reg, alignment[[1]], perl = TRUE)
